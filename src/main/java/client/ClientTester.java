8b79d42f6ec56179cc23eed95f96300dd544ebfc
Manoj <manoj.khanwalkar@gmail.com>
2015-11-26 19:31:48 -0500

    intial project setup

---
+++ src/main/java/client/ClientTester.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by mkhanwalkar on 11/26/15.
+ */
+public class ClientTester {
+}
---
+++ src/main/java/client/DCClient.java
@@ -0,0 +1,23 @@
+package client;
+
+import data.Request;
+import data.Response;
+
+/**
+ * Created by mkhanwalkar on 11/26/15.
+ */
+public class DCClient {
+
+    public Response update(Request request)
+    {
+
+        return null ;
+    }
+
+    public Response query(Request request)
+    {
+        return null ;
+    }
+
+
+}
