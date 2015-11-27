437035b90f7f04392564365052f4385a37e97ff2
Manoj <manoj.khanwalkar@gmail.com>
2015-11-26 21:06:17 -0500

    intial project setup

--- src/main/java/client/ClientTester.java
+++ src/main/java/client/ClientTester.java
@@ -1,7 +1,26 @@
 package client;
 
+import data.JSONUtil;
+import data.Request;
+import data.Response;
+import data.Type;
+
 /**
  * Created by mkhanwalkar on 11/26/15.
  */
 public class ClientTester {
+
+    public static void main(String[] args) {
+
+        DCClient client = DCClient.getInstance("localhost:100001,localhost:1002,localhost:1003");
+
+        Request request = new Request();
+        request.setKey("Hello");
+        request.setValue("World");
+        request.setType(Type.Uodate);
+
+        Response response = client.update(request);
+
+        System.out.println(JSONUtil.getJSONString(response));
+    }
 }
--- src/main/java/client/DCClient.java
+++ src/main/java/client/DCClient.java
@@ -1,23 +1,59 @@
 package client;
 
+import data.JSONUtil;
 import data.Request;
 import data.Response;
+import data.Type;
 
 /**
  * Created by mkhanwalkar on 11/26/15.
  */
 public class DCClient {
 
+    private DCClient()
+    {
+
+    }
+
     public Response update(Request request)
     {
 
-        return null ;
+        System.out.println(JSONUtil.getJSONString(request));
+        Response response = new Response();
+        response.setType(Type.UpdateResult);
+        response.setRequestId(request.getId());
+        response.setKey(request.getKey());
+        response.setValue(request.getValue());
+
+        return response ;
     }
 
     public Response query(Request request)
     {
         return null ;
     }
+
+
+    private String config ;
+
+    private void init(String config)
+    {
+
+    }
+
+    static class Holder
+    {
+        static DCClient factory = new DCClient();
+    }
+
+    public static DCClient getInstance(String config)
+    {
+        Holder.factory.init(config);
+        return Holder.factory;
+
+    }
+
+
 
 
 }
